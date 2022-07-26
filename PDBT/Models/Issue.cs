f3a6ebc21a2d769526d3aeb32e945ef18dfed222
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2022-07-26 17:36:39 +0100

    Create Issue Model

---
+++ PDBT/Models/Issue.cs
@@ -0,0 +1,30 @@
+namespace PDBT.Models;
+
+public class Issue
+{
+    public int Id { get; set; }
+    public string IssueName { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public IssueType Type { get; set; } = IssueType.Bug;
+    public IssuePriority Priority { get; set; } = IssuePriority.Medium;
+    public DateTime? TimeForCompletion { get; set; }
+    public DateTime? DueDate { get; set; }
+    public ICollection<LinkedIssue>? LinkedIssues { get; set; }
+}
+
+public enum IssuePriority
+{
+    Lowest,
+    Low,
+    Medium,
+    High,
+    Highest
+}
+
+public enum IssueType
+{
+    Bug,
+    Improvement,
+    QualityOfLife,
+    NewFeature
+}
