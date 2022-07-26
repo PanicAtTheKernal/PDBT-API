fdfaac4d5c757adf3d7be9546837f8d399c561b8
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2022-07-26 20:01:02 +0100

    Create the Issue Controller

--- PDBT/Program.cs
+++ PDBT/Program.cs
@@ -12,7 +12,10 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<PdbtContext>(opt => opt
     .UseMySql(builder.Configuration.GetConnectionString("PDBT"), serverVersion)
     .UseValidationCheckConstraints()
-    .UseEnumCheckConstraints());
+    .UseEnumCheckConstraints()
+    .LogTo(Console.WriteLine, LogLevel.Information)
+    .EnableSensitiveDataLogging()
+    .EnableDetailedErrors());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
