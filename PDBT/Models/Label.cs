7baefc8a7c84464da56b34ea08799bcb330a5682
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2022-07-26 17:40:26 +0100

    Create the Label model

--- PDBT/Models/Label.cs
+++ PDBT/Models/Label.cs
@@ -2,5 +2,7 @@ namespace PDBT.Models;
 
 public class Label
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public ICollection<Issue> Issues = null!;
 }
