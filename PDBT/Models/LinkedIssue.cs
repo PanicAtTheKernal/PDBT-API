4cab77c55b3bbc5509e0241d9e3e104115b688a6
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2022-07-26 17:49:23 +0100

    Create the LinkedIssue model

--- PDBT/Models/LinkedIssue.cs
+++ PDBT/Models/LinkedIssue.cs
@@ -2,5 +2,15 @@ namespace PDBT.Models;
 
 public class LinkedIssue
 {
-    
+    public int Id { get; set; }
+    public Issue Issue1 { get; set; } = null!;
+    public Issue Issue2 { get; set; } = null!;
+    public IssueReason Reason { get; set; } = IssueReason.Duplicate;
+}
+
+public enum IssueReason
+{
+    Blocked,
+    Linked,
+    Duplicate
 }
